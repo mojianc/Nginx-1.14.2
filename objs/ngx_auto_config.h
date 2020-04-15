908e6661e416783f414483aa46359cef2537731d
mojianc <498268626@qq.com>
2020-04-15 15:14:37 +0800

    first commit

---
+++ objs/ngx_auto_config.h
@@ -0,0 +1,6 @@
+#define NGX_CONFIGURE " --with-ld-opt=-lstdc++ --with-cpp_test_module"
+
+#ifndef NGX_COMPILER
+#define NGX_COMPILER  "gcc 5.4.0 20160609 (Ubuntu 5.4.0-6ubuntu1~16.04.12) "
+#endif
+
